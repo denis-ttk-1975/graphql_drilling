fc3f257d2e53c5d745ee8fc26e45ce39829d4d6b
denis-ttk-1975 <denis.ttk@gmail.com>
2023-08-08 13:02:29 +0300

    collection for mongoDB added

--- server/schema/data.js
+++ server/schema/data.js
@@ -1,3 +1,26 @@
+/*
+ All IDs set automatically by mLab
+ Don't forget to update after creation
+const directorsJson = [
+  { "name": "Quentin Tarantino", "age": 55 }, // 64d20f862065db1bd4827a45
+  { "name": "Michael Radford", "age": 72 }, // 64d2100d2065db1bd4827a47
+  { "name": "James McTeigue", "age": 51 }, // 64d210192065db1bd4827a49
+  { "name": "Guy Ritchie", "age": 50 }, // 64d210242065db1bd4827a4b
+];
+
+ directorId - it is ID from the directors collection
+const moviesJson = [
+  { "name": "Pulp Fiction", "genre": "Crime", "directorId": "64d20f862065db1bd4827a45" },
+  { "name": "1984", "genre": "Sci-Fi", "directorId": "64d2100d2065db1bd4827a47" },
+  { "name": "V for vendetta", "genre": "Sci-Fi-Triller", "directorId": "64d210192065db1bd4827a49" },
+  { "name": "Snatch", "genre": "Crime-Comedy", "directorId": "64d210242065db1bd4827a4b" },
+  { "name": "Reservoir Dogs", "genre": "Crime", "directorId": "64d20f862065db1bd4827a45" },
+  { "name": "The Hateful Eight", "genre": "Crime", "directorId": "64d20f862065db1bd4827a45" },
+  { "name": "Inglourious Basterds", "genre": "Crime", "directorId": "64d20f862065db1bd4827a45" },
+  { "name": "Lock, Stock and Two Smoking Barrels", "genre": "Crime-Comedy", "directorId": "64d210242065db1bd4827a4b" },
+];
+*/
+
 export const movies = [
   { id: '1', name: 'Pulp Fiction', genre: 'Crime', directorId: '1' },
   { id: '2', name: '1984', genre: 'Sci-Fi', directorId: '2' },
